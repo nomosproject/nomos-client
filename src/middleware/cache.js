f13ba4022a812dbfcf236e072fc0cc8d2d954148
Andrew Sullivan <andrew@nomos.tech>
2016-09-23 15:31:38 -0700

    Minor bugfixes

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -1,27 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 const md5 = require('blueimp-md5')
-import logger from '../../../server/logger'
 
 function removeHash (url) {
   return url.replace(/#.*$/, '')
 }
 
 function filenameForUrl (url) {
-  logger.debug(`${url} ${md5(url)} ${md5(removeHash(url))}`)
   return md5(url)
 }
 
 function retrieveFromCache (cacheDir, url) {
   const filename = path.join(cacheDir, filenameForUrl(url))
-
-  // const withoutHashFile = path.join(cacheDir, filenameForUrl(removeHash(url)))
-  // if (!fs.existsSync(withoutHashFile)) {
-  //   logger.debug('base does not exist, creating file', withoutHashFile)
-  //   fs.writeFileSync(withoutHashFile, fs.readFileSync(filename))
-  // } else {
-  //   logger.debug('base exists', withoutHashFile)
-  // }
 
   if (fs.existsSync(filename)) { // TODO: existsSync is deprecated
     const response = fs.readFileSync(filename, 'utf8')
--- src/middleware/request.js
+++ src/middleware/request.js
@@ -1,9 +1,8 @@
 const request = require('superagent')
 const _ = require('lodash')
-import logger from '../../../server/logger'
 
 function promisifiedRequest (ctx) {
-  logger.verbose('fetching', ctx.url)
+  console.log('fetching', ctx.url)
   return new Promise((resolve, reject) => {
     request.get(ctx.url).end((err, res) => {
       if (err) { return reject(err) }
